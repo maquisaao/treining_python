f2d693e25f25ed3e3b4100a6257f7b89decd2268
maquisaao <maxwspinheiro@gmail.com>
2023-03-30 14:37:28 -0300

    calculadora_calorias_diarias

--- what_the_number/computer_guess.py
+++ what_the_number/computer_guess.py
@@ -1,23 +1,26 @@
 import random
+import time
 
 
 def computer_guess(x):
     low = 1
     high = x
     feedback = ''
-    while feedback != 'c':
+    while feedback != '=':
         if low != high:
             guess = random.randint(low, high)
         else:
             guess = low
         feedback = input(
-            f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
-        if feedback == 'h':
+            f'O numero {guess} é maior (+), menor (-), or igual (=)?? ').lower()
+        if feedback == '+':
             high = guess - 1
-        elif feedback == 'l':
+        elif feedback == '-':
             low = guess + 1
 
-    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+    print(f'Issaaa! O programa adivinhou seu numero, {guess}, corretamente!')
 
+
+time.sleep(5)
 
 computer_guess(10)
--- what_the_number/person_guess.py
+++ what_the_number/person_guess.py
@@ -1,18 +1,21 @@
 import random
+import time
 
 
 def guess(x):
-    random_number = random.randint(1, 10)
+    random_number = random.randint(1, 100)
     guess = 0
     while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {x}: '))
+        guess = int(
+            input(f'Adivinha o numero que estou pensando, de 1 a {x}: '))
         if guess < random_number:
-            print('Sorry, guess again. Too low.')
+            print('Que pena, tenta de novo. Muito baixo.')
         elif guess > random_number:
-            print('Sorry, guess again. Too high.')
+            print('Que pena, tenta de novo. Muito alto.')
 
     print(
-        f'Yay, congrats. You have guessed the number {random_number} correctly!!')  # noqa: E501
+        f'Isssaa! Parabens. Você acertou o numero que eu estava pensando, é o {random_number} !!')  # noqa: E501
+    time.sleep(5)
 
 
-guess(10)
+guess(100)
